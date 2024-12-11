506f35412e246c5ae5cc4165bc2a9b76a8663203
isha123321 <isha101112@hotmail.com>
2024-12-11 21:20:49 +0500

    first commit

---
+++ a1.py
@@ -0,0 +1,16 @@
+
+def addition (a=1, b=2):
+    print(a+b)
+def subtraction (a=1 , b=2) :
+    print(a-b)   
+def multiplication (a=1 ,b=2) :
+    print(a*b)   
+def division(a=1,b=2)    :
+    print(a/b)
+addition (3,9)
+subtraction(6,3)
+multiplication(5,6)
+division(4,5)
+def modulus(a=1,b=2):
+    print(a%b)
+modulus(3,4)    
